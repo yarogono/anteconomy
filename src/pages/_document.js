9d1bf47b42190cbea53ede809104d9f1cfd7fd0e
yarogono <70641418+yarogono@users.noreply.github.com>
2025-03-01 23:35:12 +0900

    구글 애드센스 코드 추가

---
+++ src/pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="ko">
+      <Head>
+        <meta name="google-adsense-account" content="ca-pub-1811427742724427" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
