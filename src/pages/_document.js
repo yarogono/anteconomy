9bb151295a724ab2a86ad19c6b70bf0a8c2da8cd
yarogono <70641418+yarogono@users.noreply.github.com>
2025-03-02 10:09:53 +0900

    구글 애널리틱스 세팅2

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,20 +6,14 @@ export default function Document() {
       <Head>
         <meta name="google-adsense-account" content="ca-pub-1811427742724427" />
         {/* Global Site Tag (gtag.js) - Google Analytics */}
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
-            `,
-          }}
-        />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-9KJ9PH17QW"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-9KJ9PH17QW');
+        </script>
       </Head>
       <body>
         <Main />
