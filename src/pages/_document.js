50b838eb5aa231989f7f7e7a6c005e1f9332b100
yarogono <70641418+yarogono@users.noreply.github.com>
2025-03-02 10:07:35 +0900

    구글 애널리틱스 세팅 추가

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,21 @@ export default function Document() {
     <Html lang="ko">
       <Head>
         <meta name="google-adsense-account" content="ca-pub-1811427742724427" />
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
