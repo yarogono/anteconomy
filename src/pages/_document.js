db7704510ce806745bbfe5b5ce438c3d9d74c0a5
yarogono <70641418+yarogono@users.noreply.github.com>
2025-03-02 10:12:08 +0900

    구글 애널리틱스 세팅3

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,14 +6,20 @@ export default function Document() {
       <Head>
         <meta name="google-adsense-account" content="ca-pub-1811427742724427" />
         {/* Global Site Tag (gtag.js) - Google Analytics */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-9KJ9PH17QW"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-9KJ9PH17QW');
-        </script>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-9KJ9PH17QW"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-9KJ9PH17QW');
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
