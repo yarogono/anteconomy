113adc05c7a20b4673774fefaa56800a130eb660
JeonHyuck Lim(John) <naocya@naver.com>
2026-08-23 22:45:26 +0900

    구글 애널리틱스 코드 변경

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,7 +8,7 @@ export default function Document() {
         {/* Global Site Tag (gtag.js) - Google Analytics */}
         <script
           async
-          src="https://www.googletagmanager.com/gtag/js?id=G-9KJ9PH17QW"
+          src="https://www.googletagmanager.com/gtag/js?id=G-MFS6DMRF45"
         ></script>
         <script
           dangerouslySetInnerHTML={{
@@ -16,7 +16,7 @@ export default function Document() {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-9KJ9PH17QW');
+              gtag('config', 'G-MFS6DMRF45');
             `,
           }}
         />
